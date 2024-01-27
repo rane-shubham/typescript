4c984ea67e12e1d5c71e7facdb5cb96cff199f37
rane-shubham <raneshubh123@gmail.com>
2024-01-27 17:01:03 +0530

    Arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -8,3 +8,21 @@ console.log(array);
 //add fourth element to the array
 array.push('zeta');
 console.log(array);
+
+//Print all elements of an array
+array.forEach(val => console.log(val));
+
+//objects
+let objects = {
+    name: 'Tobyy',
+    gender: 'Male',
+    age: 23
+};
+console.log(objects);
+
+//union types
+let mixed: (string|number|boolean)[] = []
+mixed.push('Jade');
+mixed.push(23);
+mixed.push(true);
+console.log(mixed);
