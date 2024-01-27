e4266316e0f3106bc9476b9055df1e7df32db9ac
rane-shubham <raneshubh123@gmail.com>
2024-01-27 09:49:50 +0530

    Variables and arrays

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var naem = "Shubham";
+console.log(naem);
+//arrays
+var array = ['alpha', 'beta', 'gamma'];
+console.log(array);
+//add fourth element to the array
+array.push('zeta');
+console.log(array);
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+let naem:string = "Shubham";
+console.log(naem);
+
+//arrays
+let array = ['alpha','beta','gamma'];
+console.log(array);
+
+//add fourth element to the array
+array.push('zeta');
+console.log(array);
